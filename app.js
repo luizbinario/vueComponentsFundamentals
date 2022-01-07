b76e834119eed51869d56cf99e285cb4fc3d1f48
Luiz Carlos <luiz1987@gmail.com>
2022-01-07 10:49:11 -0300

    Reusable components with props

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-Vue.component('click-counter', {
-    template: "#click-counter-template",
-    data () {
-        return {
-            count: 0
-        }
+Vue.component('plan', {
+    template: '#plans-template',
+    props: {
+        title: String,
     }
 })
 
 new Vue({
-    el: "#app"
+    el: '#app',
+    data: {
+        plans: ["Bloodborne No Hit", "Dark Souls No Hit", "Dark Souls II No Hit", "Dark Souls III No Hit"]
+    }
 })
