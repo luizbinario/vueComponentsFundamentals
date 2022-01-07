615acd355c9fe8949194aabcdc7c6e4bec130fd7
Luiz Carlos <luiz1987@gmail.com>
2022-01-07 11:06:27 -0300

    Global vs Local Components

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-Vue.component('plan-picker', {
+let PlanComponent = {
+    template: '#plans-template',
+    props: {
+        title: String,
+    }
+}
+
+let PlaPickerComponent = {
     template: '#plan-picker-template',
+    components: {
+        'plan': PlanComponent
+    },
     data () {
         return {
             plans: ["Bloodborne No Hit", "Dark Souls No Hit", "Dark Souls II No Hit", "Dark Souls III No Hit"]
         }
     }
-})
-
-Vue.component('plan', {
-    template: '#plans-template',
-    props: {
-        title: String,
-    }
-})
+}
 
 new Vue({
-    el: '#app'
+    el: '#app',
+    components: {
+        'plan-picker': PlaPickerComponent
+    }
 })
