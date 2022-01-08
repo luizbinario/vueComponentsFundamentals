9f1b83de1697a9f837a1f9ad31d3f8cf9f763927
Luiz Carlos <luiz1987@gmail.com>
2022-01-08 12:31:16 -0300

    Component Slots

--- app.js
+++ app.js
@@ -1,52 +1,12 @@
-let PlanPickerItemComponent = {
-    template: '#plan-picker-item-template',
-    props: {
-        title: {
-            type: String
-        },
-        selectedPlan: {
-            type: String
-        }
-    },
+Vue.component('todo-item', {
+    template: '#todo-item-template',
     data() {
         return {
-            // selected: false
-        }
-    },
-    computed: {
-        isSelected() {
-            return this.title === this.selectedPlan
-        }
-    },
-    methods: {
-        select() {
-            this.$emit('select', this.title)
-            // this.selected = !this.selected
+            completed: false
         }
     }
-}
-
-let PlanPickerComponent = {
-    template: '#plan-picker-template',
-    components: {
-        'plan-picker-item': PlanPickerItemComponent
-    },
-    data () {
-        return {
-            plans: ["Bloodborne No Hit", "Dark Souls No Hit", "Dark Souls II No Hit", "Dark Souls III No Hit"],
-            selectedPlan: null
-        }
-    },
-    methods: {
-        selectPlan(plan) {
-            this.selectedPlan = plan
-        }
-    }
-}
+})
 
 new Vue({
     el: '#app',
-    components: {
-        'plan-picker': PlanPickerComponent
-    }
 })
